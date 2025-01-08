8cb5ddf5745b8d176627cd67e6c79c1eb6c4fdbf
chalasbh <chalasbh@OA-S141-08>
2025-01-08 11:23:48 -0500

    Added a script to move the square, and one to follow the mouse

---
+++ Assets/Scripts/FollowCircle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
+
+public class FollowCircle : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position =  mousePos;
+
+
+    }
+}
