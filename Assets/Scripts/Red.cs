e03da3748b67e93fb61867c3ae3dd13058120db3
Bhuvan Chalasani <chalasbh@shernet.sheridancollege.ca>
2025-01-26 19:23:03 -0500

    lerp jank fix

--- Assets/Scripts/Red.cs
+++ Assets/Scripts/Red.cs
@@ -7,11 +7,13 @@ public class Red : MonoBehaviour
     public Transform originalpos;
     public Transform newpos;
     public float t;
+    public Vector3 startPos = new Vector3(-0.008f, 0.464f,0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+
     }
 
     // Update is called once per frame
@@ -19,9 +21,11 @@ public class Red : MonoBehaviour
     {
         if (Input.GetKey("space"))
         {
-            t = Time.deltaTime;
-            transform.position = Vector3.Lerp(originalpos.position,newpos.position,t);
+            t += 0.5f*Time.deltaTime;
+            //transform.position = Vector3.Lerp(originalpos.position,newpos.position,t);
+            transform.position = Vector3.Lerp(startPos, newpos.position, t);
+            //needs animation curve
+
         }
-        
     }
 }
